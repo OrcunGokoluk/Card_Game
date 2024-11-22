94685cd4d2182e6c7de50e89ad10a179de63f32d
OrcunGokoluk <orcun.gokoluk@outlook.com>
2024-11-22 16:52:02 +0300

    Basics

---
+++ card_game.js
@@ -0,0 +1,47 @@
+
+let firstCard=10;
+let secondCard=11;
+let hasTwentyOne=false;
+let isAlive=true;
+
+let message="";
+const messageEl=document.getElementById("message-el");
+const sumEl=document.querySelector("main p:nth-child(4)");
+const cardsEl=document.getElementById("cards-el");
+
+const sum=firstCard+secondCard;
+
+
+function startGame()
+{
+    sumEl.textContent+=sum;
+    cardsEl.textContent+=firstCard+" - "+secondCard;
+
+    if(sum<21){
+        message="Do you want to draw a new card? 🙂";
+        messageEl.textContent=message;
+    }
+    else if(sum===21){
+        message="Wohoo! You've got 21! 🥳"
+        messageEl.textContent=message;
+        hasTwentyOne=true;
+    }
+    else{
+        message="You're out of the game! 😭"
+        messageEl.textContent=message;
+        isAlive=false;
+    }
+}
+
+function resetGame()
+{
+    hasTwentyOne=false;
+    isAlive=true;
+
+    sumEl.textContent="Sum: ";
+    cardsEl.textContent="Cards: ";
+
+    message="Want to play a round";
+    messageEl.textContent=message;
+}
+console.log(message);
